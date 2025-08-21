88dc52008269753c415afdd05a05f41a489c32ba
Divyasutar20 <divyasutar20102004@gmail.com>
2025-08-21 05:32:36 -0400

    Add files via upload

---
+++ arrayoperation.py
@@ -0,0 +1,33 @@
+
+i = 57
+print("signed Integer:", i, type(i))
+I = -57
+print("unsigned Integer:", I, type(I))
+l = 123456789012345678901234567890
+print("signed Long:", l, type(l))
+L= -123456789012345678901234567890
+print("unsigned Long:", L, type(L))
+q = 123456789012345678901234567890123456789012345678901234567890
+print("signed Long Long:", q, type(q))
+Q = -123456789012345678901234567890123456789012345678901234567890
+print("unsigned Long Long:", Q, type(Q))
+f = 3.14159
+print("Float:", f, type(f))
+c = 2 + 3j
+print("Complex:", c, type(c))
+b = True
+print("Boolean:", b, type(b))
+u = "Divya "
+print("Unicode String:", u, type(u))
+h= 45
+print("signed:", h, type(h))
+H= -45
+print("unsigned:", H, type(H))
+B = b"Divya Sutar"
+print("Bytes:", B, type(B))
+ba = bytearray(B)
+print("Bytearray:", ba, type(ba))
+lst = [1, 2, 3]
+print("List:", lst, type(lst))
+
+
